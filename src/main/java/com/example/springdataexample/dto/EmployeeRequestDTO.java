54ca71a45df407ddf5bc1db7cd3521377a05127c
Akshay N <akshayn@Akshays-MacBook-Pro.local>
2021-02-23 17:49:40 +0530

    mongo done

--- src/main/java/com/example/springdataexample/dto/EmployeeRequestDTO.java
+++ src/main/java/com/example/springdataexample/dto/EmployeeRequestDTO.java
@@ -3,14 +3,12 @@ package com.example.springdataexample.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Id;
 
 @Getter
 @Setter
 
 public class EmployeeRequestDTO {
 
-    @Id
     private Long id;
 
     private String name;
