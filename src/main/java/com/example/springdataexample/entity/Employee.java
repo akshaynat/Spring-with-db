52982ab0bb397b39012230f9acd59fd4c0a41b85
Akshay N <akshayn@Akshays-MacBook-Pro.local>
2021-02-24 10:07:35 +0530

    basic redis-op done

--- src/main/java/com/example/springdataexample/entity/Employee.java
+++ src/main/java/com/example/springdataexample/entity/Employee.java
@@ -2,14 +2,15 @@ package com.example.springdataexample.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
-@Document
+@RedisHash
 @Getter
 @Setter
 public class Employee {
-    @MongoId
+
+    @Id
     private Long id;
 
     private String name;
